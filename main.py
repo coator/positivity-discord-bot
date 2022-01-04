f7e9039c73714f8e624cb8c3ed983f674ad3a24f
alex w <awaite67@gmail.com>
2022-01-03 19:10:11 -0500

    task 1 completed

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import numpy as np
 import pandas as pd
 
+## TODO - Arrange part one into columns (round robin)
+## TODO - Arrange part two into columns (divide/4 and split)
+## TODO - rand function to select from columns
+## TODO - cat columns together
+## TODO - wrap and add discord components
+## TODO - create function to call 
 
+divider = '`@@@@@`'
 
-newset = pd.read_csv('raccoon-society-phrases.csv')
+itemList=[]
+for item in pd.read_csv('raccoon-society-phrases.csv'):
+    itemList.append(str(item).strip())
 
-for item in newset:
-    print(item)
+itemListDivided = [[],[],[],[]]
+count = 0
+
+## The first part of the list was done using a alternating item list as columns were read 
+## from a ocr from left to right as opposed to
+  
+for item in range(len(itemList[0:31])):
+    if count == 0 :
+        itemListDivided[0].append(itemList[item])
+        count= 1
+    elif count == 1:
+        itemListDivided[1].append(itemList[item])
+        count= 2
+    elif count == 2:
+        itemListDivided[2].append(itemList[item])
+        count = 3
+    elif count == 3:
+        itemListDivided[3].append(itemList[item])
+        count = 0
+
+[print(item) for item in itemListDivided]
